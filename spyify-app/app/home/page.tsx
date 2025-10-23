9f168950632eb946590841fac331f635a7b234c9
Anthony Zheng <ant39844@gmail.com>
2025-10-23 02:19:33 -0500

    fixes

--- spyify-app/app/home/page.tsx
+++ spyify-app/app/home/page.tsx
@@ -1,12 +1,30 @@
-import Navbar from "@/components/navbar";import FriendInfo from "@/components/friend-info";
+import Navbar from "@/components/navbar";
+import FriendInfo from "@/components/friend-info";
 export default function Home() {
   return (
     <div className="m-0">
-      <div className="m-2">
+      <div className="p-2 flex flex-col h-screen gap-4">
         <Navbar />
-      <FriendInfo friends={[{name: "John Doe", profileImage: "https://anthony39844.github.io/selfie.png", artists: ["https://anthony39844.github.io/selfie.png", "https://anthony39844.github.io/selfie.png"]}, {name: "Jane Smith", profileImage: "https://anthony39844.github.io/selfie.png", artists: ["https://anthony39844.github.io/selfie.png"]}]}/>
+        <div className="flex flex-1">
+          <FriendInfo
+            friends={[
+              {
+                name: "John Doe",
+                profileImage: "https://anthony39844.github.io/selfie.png",
+                artists: [
+                  "https://anthony39844.github.io/selfie.png",
+                  "https://anthony39844.github.io/selfie.png",
+                ],
+              },
+              {
+                name: "Jane Smith",
+                profileImage: "https://anthony39844.github.io/selfie.png",
+                artists: ["https://anthony39844.github.io/selfie.png"],
+              },
+            ]}
+          />
+        </div>
       </div>
     </div>
- 
   );
 }
